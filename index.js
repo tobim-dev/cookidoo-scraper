7173d86346bbd3871ea11409d026fd9fed35b5c7
tobim-dev <mail@tmeyer.me>
2021-01-01 20:57:43 +0100

    Added error management

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const express = require('express')
-const browserObject = require('./browser');
-const scraperController = require('./pageController');
+const express = require("express");
+const browserObject = require("./browser");
+const scraperController = require("./pageController");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.get("/recipesWeek", async (req, res) => {
+  if (!req.body.username && !req.body.password) {
+    return res
+      .status(400)
+      .send("Please provide your Cookidoo® username and password");
+  }
+  const userData = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  let browserInstance = browserObject.startBrowser();
+  const result = await scraperController(browserInstance, userData);
 
-app.post('/', async (req, res) => {
-    const userData = {
-        username: req.body.username,
-        password: req.body.password
-    }
-    let browserInstance = browserObject.startBrowser();
-    const result = await scraperController(browserInstance, userData);
-    res.send(result)
-})
+  if (!result) {
+    return res.send("Error scraping the Cookidoo website");
+  }
+
+  res.send(result);
+});
 
 app.listen(port, () => {
-  console.log(`Cookidoo Scraper listening at http://localhost:${port}`)
-})
+  console.log(`Cookidoo Scraper listening at http://localhost:${port}`);
+});
--- pageScraper.js
+++ pageScraper.js
@@ -1,44 +1,71 @@
-const getRecipeInformation = require('./recipeScraper');
+const getRecipeInformation = require("./recipeScraper");
 
 const scraperObject = {
-    url: 'https://cookidoo.de/foundation/de-DE',
-    async scraper(browser, userData){
-        let page = await browser.newPage();
-        console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url);
-        await page.waitForSelector('#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__links.unauthenticated-only > li:nth-child(2) > a');
+  url: "https://cookidoo.de/foundation/de-DE",
+  async scraper(browser, userData) {
+    let page = await browser.newPage();
+    console.log(`Navigating to ${this.url}...`);
+    await page.goto(this.url);
 
-        await page.click('#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__links.unauthenticated-only > li:nth-child(2) > a')
+    // Landing Page
+    await page.waitForSelector(
+      "#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__links.unauthenticated-only > li:nth-child(2) > a"
+    );
+    await page.click(
+      "#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__links.unauthenticated-only > li:nth-child(2) > a"
+    );
 
-        await page.waitForSelector('#email')
+    // Login Page
+    await page.waitForSelector("#email");
+    await page.$eval(
+      "#email",
+      (el, value) => (el.value = value),
+      userData.username
+    );
+    await page.$eval(
+      "#password",
+      (el, value) => (el.value = value),
+      userData.password
+    );
+    await page.click("#j_submit_id");
 
-        await page.$eval('#email', (el, value) => el.value = value, userData.username);
+    await page.waitForNavigation();
 
-        await page.waitForSelector('#email')
-        await page.$eval('#password', (el, value) => el.value = value, userData.password);
-        await page.click('#j_submit_id');
+    const error = await page.$("#j_login_form_id > div.errorMessage");
 
-
-        await page.waitForSelector('#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__main-links > li:nth-child(3) > a');
-        await page.click('#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__main-links > li:nth-child(3) > a');
-
-        await page.waitForSelector('#main > plan-timeline > section > plan-timeline-entry:nth-child(1) > article > core-tiles-list > core-tile > a')
-
-        const urls = await page.$$eval('core-tile > a', links => {
-            links = links.map(el => el.href)
-            return links;
-        });
-
-        await browser.close()
-
-        const getRecipeData = async () => {
-            return Promise.all(urls.map(url => getRecipeInformation(url)))
-        }
-
-        const recipeData = await getRecipeData()
-
-        return recipeData
+    if (error) {
+      await browser.close();
+      return null;
     }
-}
+
+    // Login Starter Page
+    await page.waitForSelector(
+      "#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__main-links > li:nth-child(3) > a"
+    );
+    await page.click(
+      "#layout--default > header > div > core-nav > nav > div.core-nav__container > ul.core-nav__main-links > li:nth-child(3) > a"
+    );
+
+    // Week Page
+    await page.waitForSelector(
+      "#main > plan-timeline > section > plan-timeline-entry:nth-child(1) > article > core-tiles-list > core-tile > a"
+    );
+
+    const urls = await page.$$eval("core-tile > a", (links) => {
+      links = links.map((el) => el.href);
+      return links;
+    });
+
+    await browser.close();
+
+    const getRecipeData = async () => {
+      return Promise.all(urls.map((url) => getRecipeInformation(url)));
+    };
+
+    const recipeData = await getRecipeData();
+
+    return recipeData;
+  },
+};
 
 module.exports = scraperObject;
