a50d10a5ef77bac5b58710f463d34c6a4dbe97c1
Tobias <mail@tmeyer.me>
2021-01-06 10:21:25 +0100

    configured jest

---
+++ src/controller/recipeInformationController.spec.ts
@@ -0,0 +1,10 @@
+// import {recipeInformationController} from './recipeInformationController'
+import recipeInformationService from 'services/recipeInformationService'
+
+jest.mock('services/recipeInformationService')
+
+describe('recipeInformationController', () => {
+  it('should ', () => {
+    expect(true).toBe(true)
+  })
+})
